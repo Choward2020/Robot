fb6c9dbec40869acb3d3e1c98f2873c27a8f3e26
chris howard <christopherhoward1997@gmail.com>
2021-11-10 09:32:46 -0700

    fight

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+var playerName = window.prompt("What is your robot's name?");
+//Not e the lack of quotation marks around playerName
+console.log(playerName);
+
+window.alert("This is an alert! JavaScript is running!");
+
+//this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+ // fight();
+
+
